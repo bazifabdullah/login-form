3b745a880d64c1884faff86ca56b3b5a97ea6bf4
Bazif Abdullah <bazifabdullah@gmail.com>
2024-03-29 02:30:45 +0500

    Project Completed

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const labels = document.querySelectorAll('.creds label');
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span>${letter}</span>`)
+        .map((letter) => `<span>${letter}</span>`)
         .join('');
 });
